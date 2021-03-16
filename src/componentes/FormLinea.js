dd3a071a90b149d76cb12f335884d0aee0a9bb36
Bernat Jorquera <bernatjv19@gmail.com>
2021-03-16 23:42:30 +0100

    Se muestra el cartel de tiempo de espera de la linea seleccionada

--- src/componentes/FormLinea.js
+++ src/componentes/FormLinea.js
@@ -4,19 +4,30 @@ import PropTypes from "prop-types";
 import ParadasContext from "../contexts/ParadasContext";
 
 function FormLinea(props) {
-  const { paradaValida } = props;
+  const { paradaValida, seleccionarRuta } = props;
   const paradas = useContext(ParadasContext).data.ibus;
   return (
     <Form hidden={paradaValida}>
       <Form.Label htmlFor="tiempo-linea">Tiempo para que llegue la línea: </Form.Label>
-      <select className="tiempo-linea" id="tiempo-linea">
+      <select
+        className="tiempo-linea"
+        id="tiempo-linea"
+        onChange={(e) => seleccionarRuta(e)}>
         <option value="">Elige línea</option>
         {
-          paradas.map(parada => <option key={parada.routeId} value={parada.line}>{parada.line}</option>)
+          paradas.map(parada => <option
+            key={parada.routeId}
+            value={parada.line}
+          >{parada.line}</option>)
         }
       </select>
     </Form>
   );
+};
+
+FormLinea.propTypes = {
+  paradaValida: PropTypes.bool.isRequired,
+  seleccionarRuta: PropTypes.func.isRequired
 };
 
 export default FormLinea;
