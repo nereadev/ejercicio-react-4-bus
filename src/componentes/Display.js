cb7b57a3cffb370ca8347ad8e9786ad5972b3955
nereadev <nereagarcia.c@gmail.com>
2021-03-16 20:31:02 +0100

    Movimiento display con css:hover + key Display

--- src/componentes/Display.js
+++ src/componentes/Display.js
@@ -8,14 +8,13 @@ const Display = () => {
         <div className="display">
             {
                 paradas.map(parada =>
-                    <div className="bus">
+                    <div key={parada.routeId} className="bus">
                         <span className="linea">{parada.line}</span>
                         <span className="destino">{parada.destination}</span>
                         <span className="tiempo">{`${parada.["t-in-min"]}min`}</span>
                     </div>
                 )
-            }
-        </div>
+            }        </div >
     );
 };
 
