1dbbf7ba98b5ad107a2272af1f4bd9e54cd88564
nereadev <nereagarcia.c@gmail.com>
2021-03-16 19:41:48 +0100

    Crear componente Display

---
+++ src/componentes/Display.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import ParadasContext from "../contexts/ParadasContext";
+
+const Display = () => {
+    const paradas = useContext(ParadasContext).data.ibus;
+
+    return (
+        <div className="display">
+            {
+                paradas.map(parada =>
+                    <div className="bus">
+                        <span className="linea">{parada.line}</span>
+                        <span className="destino">{parada.destination}</span>
+                        <span className="tiempo">{`${parada.["t-in-min"]}min`}</span>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default Display;
