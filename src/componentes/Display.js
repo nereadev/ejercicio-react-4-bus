277164aa0c1213f42c8649b99837c452d2903ee6
nereadev <nereagarcia.c@gmail.com>
2021-03-20 20:05:27 +0100

    Links de redireccion a las diferentes paginas

--- src/componentes/Display.js
+++ src/componentes/Display.js
@@ -1,19 +1,24 @@
 import { useContext } from "react";
+import { Redirect, Route, Switch } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 import ParadasContext from "../contexts/ParadasContext";
 
 const Display = () => {
   const protoparadas = useContext(ParadasContext);
   const paradas = (protoparadas.length !== 0) ? protoparadas.data.ibus : [];
+
   return (
     <div className="display">
       {
         (protoparadas.length !== 0) ?
           paradas.map(parada =>
-            <div key={parada.routeId} className="bus">
-              <span className="linea">{parada.line}</span>
-              <span className="destino">{parada.destination}</span>
-              <span className="tiempo">{`${parada["t-in-min"]}min`}</span>
-            </div>) :
+            <a href="/linea/X">
+              <div key={parada.routeId} className="bus">
+                <span className="linea">{parada.line}</span>
+                <span className="destino">{parada.destination}</span>
+                <span className="tiempo">{`${parada["t-in-min"]}min`}</span>
+              </div>
+            </a>) :
           null
       }
     </div >
